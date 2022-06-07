79803f1ebba83b3c86eec11a07281e9fa308b7b3
Shashank Acharya <acharya.s@icloud.com>
2022-06-07 19:02:10 +0800

    changed bitstamp to market history query

--- src/components/symbol-chart/ChartBody/ChartBody.js
+++ src/components/symbol-chart/ChartBody/ChartBody.js
@@ -1,23 +1,34 @@
+import { useQuery } from '@apollo/client'
 import { useEffect, useState } from 'react'
 import { BarSeries, CandlestickSeries, Chart, ChartCanvas, CrossHairCursor, EdgeIndicator, LineSeries, MouseCoordinateY, OHLCTooltip, XAxis, YAxis } from 'react-financial-charts'
+import { MARKET_HISTORY_QUERY } from '../../../queries/market-history'
 import { makeCanvasProps } from '../../../services/canvasProps'
 import createWebSocket, { finchartOnMessage } from '../../../services/createWebSocket'
-import fetchData from '../../../services/fetchData'
 import { axisStyles, candleSeriesProps, lineSeriesProps, mainChartProps } from '../../../services/mainChartProps'
 import { edgeIndicatorProps, mouseCoordinateYProps } from '../../../services/miscProps'
 import { volumeChartProps, volumeSeriesProps } from '../../../services/volumeProps'
 
 const ChartBody = ({symbol, interval, type}) => {
 	
-	const [data, setData] = useState([])
+	const [finchartData, setFinchartData] = useState([])
+
+	const { loading, error, data } = useQuery(MARKET_HISTORY_QUERY, {
+		variables: { symbol }
+	})
 	
 	useEffect(() => {
-		fetchData(symbol, interval, x => setData(x))
-		return createWebSocket(`live_trades_${symbol}`, finchartOnMessage(interval, setData))
-	}, [interval, symbol])
+		try {
+			
+			if (data !== undefined && data.getMarketHistoryForSymbol) {
+				const res = data.getMarketHistoryForSymbol.map(d => ({...d, time: new Date(parseInt(d.time))}))
+				setFinchartData(res)
+			}
+		} catch (error) {}
+		// return createWebSocket(`live_trades_${symbol}`, finchartOnMessage(interval, setFinchartData))
+	}, [interval, symbol, loading, error, data])
 	
 	return (
-		<ChartCanvas {...makeCanvasProps(data)}>
+		<ChartCanvas {...makeCanvasProps(finchartData)}>
 			<Chart {...volumeChartProps}>
 				<BarSeries {...volumeSeriesProps}/>
 			</Chart>
--- src/services/scaleProvider.js
+++ src/services/scaleProvider.js
@@ -1,7 +1,7 @@
 import { discontinuousTimeScaleProviderBuilder } from "react-financial-charts";
 
 const scaleProvider = discontinuousTimeScaleProviderBuilder().inputDateAccessor(
-    (d) => new Date(d.date)
+    (d) => new Date(d.time)
 );
 
 export default scaleProvider
