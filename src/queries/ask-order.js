d3d6d228f5309bab8202cc6fadbe848e4fc54760
BOTShiZJ <zhijian_shi@163.com>
2022-07-15 22:08:06 +0800

    Simplify open order subscription

--- src/queries/ask-order.js
+++ src/queries/ask-order.js
@@ -3,16 +3,8 @@ import { gql } from "@apollo/client";
 export const ASK_ORDER_BOOK_QUERY = gql`
     query GetOpenAskOrdersForSymbol($symbol: String!) {
         getOpenAskOrdersForSymbol(symbol: $symbol) {
-            orderid
-            walletid
-            owner
-            quantity
-            symbol
             price
             openquantity
-            fillcost
-            createdat
-            updatedat
         }
     }
 `
@@ -20,16 +12,8 @@ export const ASK_ORDER_BOOK_QUERY = gql`
 export const ASK_ORDER_BOOK_SUBSCRIPTION = gql`
     subscription OnAskOrderUpdate($symbol: String!) {
         newAskOpenOrder(symbol: $symbol) {
-            orderid
-            walletid
-            owner
-            quantity
-            symbol
             price
             openquantity
-            fillcost
-            createdat
-            updatedat
         }
     }
 `
--- src/queries/bid-order.js
+++ src/queries/bid-order.js
@@ -3,16 +3,8 @@ import { gql } from "@apollo/client";
 export const BID_ORDER_BOOK_QUERY = gql`
     query GetOpenBidOrdersForSymbol($symbol: String!) {
         getOpenBidOrdersForSymbol(symbol: $symbol) {
-            orderid
-            walletid
-            owner
-            quantity
-            symbol
             price
             openquantity
-            fillcost
-            createdat
-            updatedat
         }
     }
 `
@@ -20,16 +12,8 @@ export const BID_ORDER_BOOK_QUERY = gql`
 export const BID_ORDER_BOOK_SUBSCRIPTION = gql`
     subscription OnBidOrderUpdate($symbol: String!) {
         newBidOpenOrder(symbol: $symbol) {
-            orderid
-            walletid
-            owner
-            quantity
-            symbol
             price
             openquantity
-            fillcost
-            createdat
-            updatedat
         }
     }
 `
