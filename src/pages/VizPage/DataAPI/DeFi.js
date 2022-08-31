edb83ebe4cda91b7265e0304df55252dcd059528
Pitchappan P RM <46064869+Pitch2342@users.noreply.github.com>
2022-08-31 21:40:43 +0530

    Added Base DeFi Viz
    Added Vertical Bar Chart to show top 6 Decentralized Finance Ecosystems

--- src/pages/VizPage/DataAPI/DeFi.js
+++ src/pages/VizPage/DataAPI/DeFi.js
@@ -3,25 +3,22 @@ import React, {useState, useEffect} from "react";
 import { coinGecko} from "./APIHelpers/endpoints";
 import { http, coinGecko_API} from "./APIHelpers/APIconfig";
 import { toCamelCase } from "./FormatHelpers/CaseHandler";
-import MarketCapBarChart from "./VizComponents/MarketCapBar";
 import { retrieveCache, cacheWithExpiry } from "./APIHelpers/Cachers";
+import DeFiDonut from "./VizComponents/DefiDonut";
 
 export default function DeFi() {
-  const [DeFiCap, setDeFiCap] = useState(null);
   const [DeFiData, setDeFiCapData] = useState(null);
 
   useEffect(() => {
     async function getDeFiCapData() {
 
       const cachedDeFiCapData = retrieveCache('DeFiCapData');
-      const cachedDeFiCap = retrieveCache('DeFiCap');
 
-      if (cachedDeFiCap) {
-        setDeFiCap(cachedDeFiCap)
-        setDeFiCapData(cachedDeFiCapData)
-        console.log("Used DeFi Cap cache")
-      }
-      else {
+      // if (cachedDeFiCapData) {
+      //   setDeFiCapData(cachedDeFiCapData)
+      //   console.log("Used DeFi Cap cache")
+      // }
+      // else {
         const canceler = axios.CancelToken.source();
 
         const response = await http.request({
@@ -30,40 +27,41 @@ export default function DeFi() {
               cancelToken: canceler.token
             });
 
-        const globalDeFi = await http.request({
-                ...coinGecko_API,
-                url: coinGecko.defi_global,
-                cancelToken: canceler.token
-        });
-
-        const global_defi_cap = globalDeFi.data.data.defi_market_cap
-
-        console.log(global_defi_cap)
-        
         const normalizedResponse = toCamelCase(response.data);
   
-        console.log("DEFI : ",normalizedResponse)
-  
-        // cacheWithExpiry('DeFiCap', DeFiCap, 1200000);  // Cache Period: 20 minutes
-        // cacheWithExpiry('DeFiCapData', newData, 1200000); 
-        // console.log("DeFiCapData Fetched")
-        // setDeFiCap(DeFiCap)
-        // setDeFiCapData(newData)
-      }
+        // console.log("DEFI : ",normalizedResponse)
+        
+        const N = 6
 
-      
-      
+        const topNdefi = normalizedResponse.slice(0, N);
+
+        const defiData = []
+
+        topNdefi.forEach(element => {
+          defiData.push({
+            x: element.name,
+            y: element.marketCap,
+          })
+        });
+
+        console.log(defiData)
+  
+        cacheWithExpiry('DeFiCapData', defiData, 1200000); 
+        console.log("DeFiCapData Fetched")
+        setDeFiCapData(defiData)
+      // }
+
     }
 
     getDeFiCapData();
 
   }, []);
 
-  if (!DeFi) return "Data Loading"
+  if (!DeFiData) return "Data Loading"
 
   return(
     <div>
-        DEFI
+        <DeFiDonut Data={DeFiData}/>
     </div>
   );
 }
---
+++ src/pages/VizPage/DataAPI/VizComponents/DefiDonut.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import Chart from "react-apexcharts";
+import { Stack, Card, Text, Divider} from "@mantine/core";
+
+export default function DeFiDonut({Data}) {
+
+  const newData = Data
+
+  console.log("ND",newData)
+
+  const chartState = {
+      
+    MarketSeries: [{
+        data: newData
+    }],
+    MarketOptions: {
+        chart: {
+            id: "DeFi",
+            group: "DeFi",
+            type: 'bar',
+            height: "100%",
+            width: "100%"
+        },
+        title: {
+            text: 'Top 6 DeFi Ecosystems',
+            align: 'left'
+        },
+        yaxis: {
+          // categories: names
+          labels: {
+            formatter: function (value) {
+              return parseInt(value/1000000000) + "B$";
+            }
+          },
+        },
+        dataLabels: {
+          enabled: false
+        },
+        fill: {
+            type: 'gradient' 
+          },
+        legend: {
+          show: false
+        },
+        tooltip: {
+            custom: function({series, seriesIndex, dataPointIndex, w}) {
+              var data = w.globals.initialSeries[seriesIndex].data[dataPointIndex];
+              
+              return '<ul>' +
+              '<li><b>Ecosystem</b>: ' + data.x + '</li>' +
+              '<li><b>Value</b>: ' + parseFloat(data.y/1000000000).toFixed(3).toString() +' B $ </li>' +
+              '</ul>';
+            }
+        },
+    },
+
+    };
+
+  return (
+    // <div>
+    //     <div>Crypto Market Cap : {parseFloat(globalCap/1000000000000).toFixed(3)} Trillion USD</div>
+    //     <div id='chart_hash'>
+    //         <Chart
+    //           options={chartState.MarketOptions}
+    //           series={chartState.MarketSeries}
+    //           type="bar"
+    //           width="100%"
+    //         />
+    //     </div>
+    // </div>
+    <Card>
+        <Chart
+              options={chartState.MarketOptions}
+              series={chartState.MarketSeries}
+              type="bar"
+              width="100%"
+        />	
+	</Card>
+    
+  )
+}
